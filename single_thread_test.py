eced4b1f706f239d0748848aff6c8cf429ff63d1
Bob Copeland <me@bobcopeland.com>
2011-06-28 09:54:07 -0400

    Use the new API in the single_thread_test.py

--- single_thread_test.py
+++ single_thread_test.py
@@ -3,50 +3,48 @@ from pybase import *
 from hbase.ttypes import *
 
 if __name__ == '__main__':
-    from hashlib import md5
     import progressbar
 
     TABNAME = 'pycassahtable'
-    KEYS=int(1e2)
+    KEYS=256
     client = connect_thread_local(['localhost:9090'])
     client.getTableNames()
-    tab = HTable(client, TABNAME,[ColumnDescriptor(name='foo:'), ColumnDescriptor(name='foo2:')], createIfNotExist=True, overwrite=True)
-    print "TABELLA", tab
+    tab = HTable(client, TABNAME,
+        [ColumnDescriptor(name='foo:'), ColumnDescriptor(name='foo2:')],
+        createIfNotExist=True, overwrite=True)
+    print "Table: ", tab
 
-    h = md5()
     widgets = ['Insert Values: ', progressbar.Percentage(), ' ', progressbar.Bar('>'), ' ', progressbar.ETA(), ' ', progressbar.FileTransferSpeed()]
     prog = progressbar.ProgressBar(widgets = widgets, maxval=KEYS).start()
     for i in range(KEYS):
-        h.update("%d" % i)
-        row=h.digest()
+        row_key = "%.8x" % i
         value = "%s" % i
         mods = {"foo:ciao":value, "foo:ciao2":value}
-        tab.insert(row, mods)
-        #tab.insert(row=row, column="foo:ciao", value="%s" % i)
+        tab.insert(row_key, mods)
         prog.update(i)
 
     print "Testing Scanner"
+
+    start = "%.8x" % (int(KEYS / 2))
+    end = "%.8x" % (int(3 * KEYS / 4))
+
     print "Scan all rows"
-    it = tab.scanner()
-    for i in it:
+    for i in tab.get_range():
         print i
     print
 
-    print "Scan starting from {0}".format(repr('\xd9dd\xc96F\x9cq\xe2b\xf9\xdf\xd3\x88J\xfe'))
-    it = tab.scanner(startRow='\xd9dd\xc96F\x9cq\xe2b\xf9\xdf\xd3\x88J\xfe')
-    for i in it:
+    print "Scan starting from %s" % start
+    for i in tab.get_range(start):
         print i
     print
 
-    print "Scan starting from {0} to {1}".format(repr('\xd9dd\xc96F\x9cq\xe2b\xf9\xdf\xd3\x88J\xfe'), repr('\xe5Q\xd2\xc1\xf4 6\xac\xb4\\\xadt\x19d\xe74'))
-    it = tab.scanner(startRow='\xd9dd\xc96F\x9cq\xe2b\xf9\xdf\xd3\x88J\xfe', stopRow='\xe5Q\xd2\xc1\xf4 6\xac\xb4\\\xadt\x19d\xe74')
-    for i in it:
+    print "Scan starting from %s to %s" % (start, end)
+    for i in tab.get_range(start, end):
         print i
     print
 
-    print "Scan starting from {0} to {1} column='foo:ciao'".format(repr('\xd9dd\xc96F\x9cq\xe2b\xf9\xdf\xd3\x88J\xfe'), repr('\xe5Q\xd2\xc1\xf4 6\xac\xb4\\\xadt\x19d\xe74'))
-    it = tab.scanner(startRow='\xd9dd\xc96F\x9cq\xe2b\xf9\xdf\xd3\x88J\xfe', stopRow='\xe5Q\xd2\xc1\xf4 6\xac\xb4\\\xadt\x19d\xe74', columnlist=['foo:ciao'])
-    for i in it:
+    print "Scan starting from %s to %s column='foo:ciao'" % (start, end)
+    for i in tab.get_range(start, end, columns=['foo:ciao']):
         print i
     print
 
